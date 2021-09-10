ee16fa40a3e0feea39f6d6649efde7e711e5837b
Roberto Bayona <rbayona03@gmail.com>
2021-09-09 23:17:38 -0400

    Intial Commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavComp from './NavComp';
+
+class App extends React.Component {
+
+    render(){
+        return(
+           
+            <div>
+                 <NavComp />
+                WED-nesday App
+
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/NavComp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class NavComp extends React.Component {
+
+    render(){
+        return(
+            <div>This Will Be the Header</div>
+        )
+    }
+}
+
+export default NavComp;
