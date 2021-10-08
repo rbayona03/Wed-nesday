c3089e61826b95c467ea96bd8ce003330691106c
Roberto Bayona <rbayona03@gmail.com>
2021-10-08 00:45:46 -0400

    Location component content mockup

--- src/components/Location.js
+++ src/components/Location.js
@@ -4,7 +4,16 @@ import '../Styles/styles.css';
 
 const Location = () => {
     return(
-        <div className="locationSection">When & Where</div>
+        <div>
+                <div className="locationSection">When & Where
+                    <div className="eventDate">
+                        <h1>December 17, 2022</h1>
+                        <p>Janesy & Robert are having a celebration will be held at the thallata estate located in ...(location info)</p>
+                    </div>
+                    <div className="bgBanner">Janesy & Rob are having a celebration</div>
+                </div>
+        </div>
+
     )
 };
 
